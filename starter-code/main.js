6fe01ec1ef64b9e81085d5e63ef87d377354ea15
Emelinur <emelinuryzgc@gmail.com>
2026-01-21 17:53:04 +0300

    feat: finalize UI structure and setup JS entry point

---
+++ starter-code/main.js
@@ -0,0 +1,7 @@
+const passwordGenText=document.querySelector(".password-gen_text");
+const copyBtn=document.querySelector(".password-gen_copy-btn");
+const characterLengthSlider=document.querySelector(".character-length_slider");
+const upperCase=document.querySelector("#uppercase");
+const lowerCase=document.querySelector("#lowercase");
+const numbers=document.querySelector("#numbers");
+const symbols=document.querySelector("#symbols");
