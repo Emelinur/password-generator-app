89b7c3c94063b189cf7f873454096ca6de1a0fac
Emelinur <emelinuryzgc@gmail.com>
2026-01-22 15:01:38 +0300

    feat: update HTML structure and initialize JS variables

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,7 +1,25 @@
 const passwordGenText=document.querySelector(".password-gen_text");
 const copyBtn=document.querySelector(".password-gen_copy-btn");
 const characterLengthSlider=document.querySelector(".character-length_slider");
-const upperCase=document.querySelector("#uppercase");
-const lowerCase=document.querySelector("#lowercase");
-const numbers=document.querySelector("#numbers");
-const symbols=document.querySelector("#symbols");
+const uppercaseCheckbox=document.querySelector("#uppercase");
+const lowerCaseCheckbox=document.querySelector("#lowercase");
+const numbersCheckbox=document.querySelector("#numbers");
+const symbolsCheckbox=document.querySelector("#symbols");
+const strengthMeterText=document.querySelector(".strength-meter_text");
+const strengthMeterBars=document.querySelector(".strength-meter_bars");
+const generateButton=document.querySelector(".generate-btn");
+
+
+const uppercaseChars="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercaseChars="abcdefghijklmnopqrstuvwxyz";
+const numberChars = "0123456789";
+const symbolsChars="!@#$%^&*()_+~`|}{[]:;?><,./-=";
+
+copyBtn.addEventListener("click",()=>{
+ let copy= passwordGenText.value
+ console.log(copy)
+})
+
+generateButton.addEventListener("click", ()=>{
+console.log(passwordGenText.value);
+})
